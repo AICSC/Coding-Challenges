9819599afb8308f5a38c6aa4be21c919595e3a38
Diego Diaz <diego.diaz1994@email.bakersfieldcollege.edu>
2015-09-18 10:41:24 -0700

    Create BottlesOfBeer.cpp

---
+++ 2015/09-18/Diego/BottlesOfBeer.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+int main(int arc, char* argv[]){   
+   for(int bottles = 100; --bottles >= 0; cout << bottles << " bottles of beer on the wall." << endl) {}                              
+}
